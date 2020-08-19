23823fc723a6c43b119ea37ac7d60b385553f41f
Daniel Lindote <daniel.lindote@gmail.com>
2020-08-19 17:44:25 +0100

    add file

---
+++ logicpos.financial/logicpos.financial.library/App/SettingsApp.cs
@@ -0,0 +1,12 @@
+﻿namespace logicpos.financial.library.App
+{
+    /// <summary>
+    /// Warning this is a Private Key Must be Private for "*.softwarevendor.plugin"
+    /// this SecretKey are paired with VenderPlugin SecretKey
+    /// </summary>
+    public class SettingsApp : logicpos.shared.App.SettingsApp
+    {
+        // internal 
+        public static string SecretKey = ")p[r#HW'gOg|KNI1L3k]H&~D!DKy`Y[fx2/t&s7{:!S<xDl,l#5)[YHcVf'3UUc";
+    }
+}
