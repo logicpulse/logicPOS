f2a5bebb72a3b3e8500c457b189976d398008a78
Chialo Armando <96700865+Tchial0@users.noreply.github.com>
2024-05-08 10:32:57 +0100

    Add ResourceNames

--- LogicPOS.Globalization/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LogicPOS.Globalization
-{
-    public static class Constants
-    {
-        public const string APP_ERROR_APPLICATION_UNLICENSED_FUNCTION_DISABLED = "app_error_application_unlicenced_function_disabled";
-    }
-}
