9f821f5ceb76a4348660a95499ec095a7d7ff68a
Chialo Armando <96700865+Tchial0@users.noreply.github.com>
2024-05-16 11:31:03 +0100

    Small changes

--- LogicPOS.Settings/Common/DatabaseSettings.cs
+++ LogicPOS.Settings/Common/DatabaseSettings.cs
@@ -1,4 +1,7 @@
 ﻿using LogicPOS.Settings.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LogicPOS.Settings
 {
@@ -9,5 +12,36 @@ namespace LogicPOS.Settings
         public static string DatabaseName { get; set; }
         public static string DatabaseUser { get; set; }
         public static string DatabasePassword { get; set; }
+
+        public static void AssignConnectionStringToSettings(string connectionString)
+        {
+            Dictionary<string, string> connectionStringToDictionary = GetDictionaryFromConnectionString(connectionString);
+
+            switch (DatabaseType)
+            {
+                case DatabaseType.SQLite:
+                case DatabaseType.MonoLite:
+                    break;
+                case DatabaseType.MSSqlServer:
+                    DatabaseServer = connectionStringToDictionary["Data Source"];
+                    DatabaseUser = connectionStringToDictionary["User ID"];
+                    DatabasePassword = connectionStringToDictionary["Password"];
+                    break;
+                case DatabaseType.MySql:
+                    DatabaseServer = connectionStringToDictionary["server"];
+                    DatabaseUser = connectionStringToDictionary["user id"];
+                    DatabasePassword = connectionStringToDictionary["password"];
+                    break;
+            }
+        }
+
+        private static Dictionary<string, string> GetDictionaryFromConnectionString(string connectionString)
+        {
+            Dictionary<string, string> connStringParts = connectionString.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+              .Select(t => t.Split(new char[] { '=' }, 2))
+              .ToDictionary(t => t[0].Trim(), t => t[1].Trim(), StringComparer.InvariantCultureIgnoreCase);
+
+            return connStringParts;
+        }
     }
 }
--- LogicPOS.Utility/Utils/StringUtils.cs
+++ LogicPOS.Utility/Utils/StringUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -190,5 +191,6 @@ namespace LogicPOS.Utility
 
             return result;
         }
+
     }
 }
