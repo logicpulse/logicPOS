1ecb9cb9607c4e13c3a83afce59e632fc74eb793
Chialo Armando <96700865+Tchial0@users.noreply.github.com>
2024-05-27 16:27:36 +0100

    Rename Projects

--- LogicPOS.Data/Properties/AssemblyInfo.cs
+++ LogicPOS.Data/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogicposDatalayer")]
+[assembly: AssemblyTitle("LogicPOS.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Logicpulse")]
-[assembly: AssemblyProduct("LogicposDatalayer")]
+[assembly: AssemblyProduct("LogicPOS")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
