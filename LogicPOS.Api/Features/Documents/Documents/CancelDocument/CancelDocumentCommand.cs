46d7b7f13ee3f45ff823353512518d23d1146e46
LucianoSMS <yanilson1996@gmail.com>
2024-10-02 11:34:49 +0100

    Create CancelDocument feature on Api project

---
+++ LogicPOS.Api/Features/Documents/Documents/CancelDocument/CancelDocumentCommand.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+
+namespace LogicPOS.Api.Features.Documents.CancelDocument
+{
+    public class CancelDocumentCommand : IRequest<ErrorOr<Unit>>
+    {
+        public Guid Id { get; set; }
+        public string Reason { get; set; }
+    }
+}
---
+++ LogicPOS.Api/Features/Documents/Documents/CancelDocument/CancelDocumentCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using ErrorOr;
+using LogicPOS.Api.Features.Common;
+using MediatR;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogicPOS.Api.Features.Documents.CancelDocument
+{
+    public class CancelDocumentCommandHandler :
+        RequestHandler<CancelDocumentCommand, ErrorOr<Unit>>
+    {
+        public CancelDocumentCommandHandler(IHttpClientFactory factory) : base(factory)
+        {
+        }
+
+        public override async Task<ErrorOr<Unit>> Handle(CancelDocumentCommand command,
+                                                   CancellationToken cancellationToken = default)
+        {
+            return await HandleUpdateCommandAsync($"documents/{command.Id}/cancel", command, cancellationToken);
+        }
+    }
+}
