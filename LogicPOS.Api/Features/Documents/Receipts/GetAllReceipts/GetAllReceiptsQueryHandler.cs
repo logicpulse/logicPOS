d6898637ce3c379a3169c109f527ba733e49b78a
Chialo Armando <96700865+Tchial0@users.noreply.github.com>
2024-10-10 09:22:10 +0100

    Refactor project and add receipt handling features
    Removed DialogTemplate.cs and cleaned up LogicPOS.UI.csproj:
    - Updated Spire.Pdf library reference
    - Removed various content, resource inclusions, and project references
    - Removed WCF metadata, web reference URL, and analyzers
    - Removed package references for ClosedXML and DevExpress.Xpo
    
    Added receipt handling in LogicPOS.Api:
    - Added Receipt.cs defining Receipt class
    - Added GetAllReceiptsQuery.cs for querying receipts
    - Added GetAllReceiptsQueryHandler.cs for handling receipt queries

---
+++ LogicPOS.Api/Features/Documents/Receipts/GetAllReceipts/GetAllReceiptsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using ErrorOr;
+using LogicPOS.Api.Entities;
+using LogicPOS.Api.Features.Common;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogicPOS.Api.Features.Receipts.GetAllReceipts
+{
+    public class GetAllReceiptsQueryHandler :
+        RequestHandler<GetAllReceiptsQuery, ErrorOr<IEnumerable<Receipt>>>
+    {
+        public GetAllReceiptsQueryHandler(IHttpClientFactory factory) : base(factory)
+        {
+        }
+
+        public async override Task<ErrorOr<IEnumerable<Receipt>>> Handle(GetAllReceiptsQuery request, CancellationToken cancellationToken = default)
+        {
+            return await HandleGetAllQueryAsync<Receipt>("receipts", cancellationToken);
+        }
+    }
+}
---
+++ LogicPOS.Api/Features/Documents/Receipts/Receipt.cs
@@ -0,0 +1,32 @@
+﻿using LogicPOS.Api.Features.Common;
+using System;
+
+namespace LogicPOS.Api.Entities
+{
+    public class Receipt : ApiEntity
+    {
+        public PaymentMethod PaymentMethod { get; set; }
+        public Guid PaymentMethodId { get; set; }
+        public string HashCode { get; set; }
+        public string Hash4Code { get; set; }
+        public string RefNo { get; set; }
+        public string TransactionID { get; set; }
+        public string TransactionDate { get; set; }
+        public string Status { get; set; }
+        public string StatusDate { get; set; }
+        public string StatusReason { get; set; }
+        public string StatusSourceID { get; set; }
+        public string SourcePayment { get; set; }
+        public string Mechanism { get; set; }
+        public decimal Amount { get; set; }
+        public string Date { get; set; }
+        public string SourceID { get; set; }
+        public string SystemEntryDate { get; set; }
+        public decimal TaxPayable { get; set; }
+        public string CurrencyCode { get; set; }
+        public decimal CurrencyAmount { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public decimal WithholdingTaxAmount { get; set; }
+        public string ExtendedValue { get; set; }
+    }
+}
