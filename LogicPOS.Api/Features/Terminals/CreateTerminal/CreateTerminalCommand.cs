e95aaf9e02cae2d9b80b2b1526bf397d7bebbad1
LucianoSMS <yanilson1996@gmail.com>
2024-11-04 11:03:02 +0100

    Add CreateTerminal Feature in Api

---
+++ LogicPOS.Api/Features/Terminals/CreateTerminal/CreateTerminalCommand.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using MediatR;
+using System;
+
+namespace LogicPOS.Api.Features.Terminals.CreateTerminal
+{
+    public class CreateTerminalCommand : IRequest<ErrorOr<Guid>>
+    {
+    }
+}
---
+++ LogicPOS.Api/Features/Terminals/CreateTerminal/CreateTerminalCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+using LogicPOS.Api.Features.Common;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogicPOS.Api.Features.Terminals.CreateTerminal
+{
+    public class CreateTerminalCommandHandler : 
+        RequestHandler<CreateTerminalCommand, ErrorOr<Guid>>
+    {
+        public CreateTerminalCommandHandler(IHttpClientFactory factory) : base(factory)
+        {
+        }
+
+        public override async Task<ErrorOr<Guid>> Handle(CreateTerminalCommand command, CancellationToken cancellationToken = default)
+        {
+            return await HandleAddCommandAsync("terminals/create", command, cancellationToken);
+        }
+    }
+}
