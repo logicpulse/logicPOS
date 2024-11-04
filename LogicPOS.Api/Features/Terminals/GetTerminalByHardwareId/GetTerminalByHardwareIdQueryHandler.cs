e3fbe83c5862c4ad939e043281d1387a5c51f56e
LucianoSMS <yanilson1996@gmail.com>
2024-11-04 11:46:28 +0100

    Add GetTerminalByHardwareId Feature

---
+++ LogicPOS.Api/Features/Terminals/GetTerminalByHardwareId/GetTerminalByHardwareIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+using LogicPOS.Api.Entities;
+using LogicPOS.Api.Features.Common;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogicPOS.Api.Features.Terminals.GetTerminalByHardwareId
+{
+    public class GetTerminalByHardwareIdQueryHandler :
+        RequestHandler<GetTerminalByHardwareIdQuery, ErrorOr<Terminal>>
+    {
+        public GetTerminalByHardwareIdQueryHandler(IHttpClientFactory factory) : base(factory)
+        {
+        }
+
+        public async override Task<ErrorOr<Terminal>> Handle(GetTerminalByHardwareIdQuery query, CancellationToken cancellationToken = default)
+        {
+            return await HandleGetEntityQueryAsync<Terminal>($"terminals/hardwareid/{query.HardwareId}", cancellationToken);
+        }
+    }
+}
