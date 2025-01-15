6e78539160f2851b90dd8ec90fb74f63f4c0aaa2
Chialo Armando <96700865+Tchial0@users.noreply.github.com>
2025-01-15 14:12:48 +0100

    Add ESC-POS-USB-NET package and ThermalPrintingService class
    Updated LogicPOS.UI.csproj to include ESC-POS-USB-NET v1.1.3 for handling ESC/POS commands over USB, typically for thermal printers. Added a new ThermalPrintingService class in LogicPOS.UI.Printing.Services namespace, currently empty but with necessary using directives for future development.

---
+++ LogicPOS.UI/Printing/Thermal/ThermalPrintingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicPOS.UI.Printing.Services
+{
+    internal class ThermalPrintingService
+    {
+    }
+}
