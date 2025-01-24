98dbd7b9280d9579a219f4e247db4823c9734921
Chialo Armando <96700865+Tchial0@users.noreply.github.com>
2025-01-24 09:12:09 +0100

    Remove SaftSettings class and related properties/methods
    The `SaftSettings` class and its associated properties and methods
    have been removed from the `LogicPOS.Settings` namespace. This
    includes properties such as `DocumentsPadLength`,
    `FinanceFinalConsumerFiscalNumber`, `FinanceFinalConsumerFiscalNumberDisplay`,
    `SaftProductCompanyTaxID`, `SaftSoftwareCertificateNumber`,
    `SaftVersionPrefix`, `SaftVersion`, `SaftSoftwareCertificateNumberAO`,
    `SaftVersionPrefixAO`, `SaftVersionAO`, and `TaxAccountingBasis`.
    Additionally, the methods `GetSaftProductID` and `GetSaftProductID_AO`
    have been removed.
    
    Corresponding lines in `PluginSettings.cs` that were setting values
    for these `SaftSettings` properties using the `GetSoftwareVendorValueAsString`
    and `GetSoftwareVendorValueAsInt` methods have also been removed.

--- LogicPOS.Settings/Common/PluginSettings.cs
+++ LogicPOS.Settings/Common/PluginSettings.cs
@@ -64,23 +64,12 @@ namespace LogicPOS.Settings
             CultureSettings.FileFormatSaftPT = GetSoftwareVendorValueAsString(nameof(CultureSettings.FileFormatSaftPT));
             CultureSettings.FileFormatSaftAO = GetSoftwareVendorValueAsString(nameof(CultureSettings.FileFormatSaftAO));
 
-            SaftSettings.DocumentsPadLength = GetSoftwareVendorValueAsInt(nameof(SaftSettings.DocumentsPadLength));
             CultureSettings.DateTimeFormatDocumentDate = GetSoftwareVendorValueAsString(nameof(CultureSettings.DateTimeFormatDocumentDate));
             CultureSettings.DateTimeFormatCombinedDateTime = GetSoftwareVendorValueAsString(nameof(CultureSettings.DateTimeFormatCombinedDateTime));
-            SaftSettings.FinanceFinalConsumerFiscalNumber = GetSoftwareVendorValueAsString(nameof(SaftSettings.FinanceFinalConsumerFiscalNumber));
-            SaftSettings.FinanceFinalConsumerFiscalNumberDisplay = GetSoftwareVendorValueAsString(nameof(SaftSettings.FinanceFinalConsumerFiscalNumberDisplay));
             CultureSettings.DecimalFormatSAFTPT = GetSoftwareVendorValueAsString(nameof(CultureSettings.DecimalFormatSAFTPT));
             CultureSettings.DecimalFormatSAFTAO = GetSoftwareVendorValueAsString(nameof(CultureSettings.DecimalFormatSAFTAO));
             CultureSettings.DecimalFormatGrossTotalSAFTPT = GetSoftwareVendorValueAsString(nameof(CultureSettings.DecimalFormatGrossTotalSAFTPT));
             CultureSettings.DecimalRoundTo = GetSoftwareVendorValueAsInt(nameof(CultureSettings.DecimalRoundTo));
-            SaftSettings.SaftProductCompanyTaxID = GetSoftwareVendorValueAsString(nameof(SaftSettings.SaftProductCompanyTaxID));
-            SaftSettings.SaftSoftwareCertificateNumber = GetSoftwareVendorValueAsString(nameof(SaftSettings.SaftSoftwareCertificateNumber));
-            SaftSettings.SaftSoftwareCertificateNumberAO = GetSoftwareVendorValueAsString(nameof(SaftSettings.SaftSoftwareCertificateNumberAO));
-            SaftSettings.SaftVersionPrefix = GetSoftwareVendorValueAsString(nameof(SaftSettings.SaftVersionPrefix));
-            SaftSettings.SaftVersionPrefixAO = GetSoftwareVendorValueAsString(nameof(SaftSettings.SaftVersionPrefixAO));
-            SaftSettings.SaftVersion = GetSoftwareVendorValueAsString(nameof(SaftSettings.SaftVersion));
-            SaftSettings.SaftVersionAO = GetSoftwareVendorValueAsString(nameof(SaftSettings.SaftVersionAO));
-            SaftSettings.TaxAccountingBasis = GetSoftwareVendorValueAsString(nameof(SaftSettings.TaxAccountingBasis));
             CultureSettings.SaftCurrencyCode = GetSoftwareVendorValueAsString(nameof(CultureSettings.SaftCurrencyCode));
             CultureSettings.SaftCurrencyCodeAO = GetSoftwareVendorValueAsString(nameof(CultureSettings.SaftCurrencyCodeAO));
         }
