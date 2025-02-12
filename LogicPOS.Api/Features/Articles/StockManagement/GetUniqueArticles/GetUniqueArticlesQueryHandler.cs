06c36e1cbc14bc41a124e9511930fb291eb4deef
LucianoSMS <yanilson1996@gmail.com>
2025-02-12 17:05:55 +0100

    Implement Unique Articleson Api project

---
+++ LogicPOS.Api/Features/Articles/StockManagement/GetUniqueArticles/GetUniqueArticlesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using ErrorOr;
+using LogicPOS.Api.Entities;
+using LogicPOS.Api.Features.Articles.StockManagement.GetUniqueArticles;
+using LogicPOS.Api.Features.Common;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogicPOS.Api.Features.Articles.StockManagement.GetArticleByCode
+{
+    public class GetUniqueArticlesQueryHandler :
+        RequestHandler<GetUniqueArticlesQuery, ErrorOr<IEnumerable<WarehouseArticle>>>
+    {
+        public GetUniqueArticlesQueryHandler(IHttpClientFactory factory) : base(factory)
+        {
+        }
+
+        public async override Task<ErrorOr<IEnumerable<WarehouseArticle>>> Handle(GetUniqueArticlesQuery query, CancellationToken cancellationToken = default)
+        {
+            return await HandleGetAllQueryAsync<WarehouseArticle>($"articles/{query.Id}/unique-articles", cancellationToken);
+        }
+    }
+}
