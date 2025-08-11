f51f76e3c41dc122b51b60ecb7efff095f9e7871
tchial0 <96700865+Tchial0@users.noreply.github.com>
2025-08-11 13:43:51 +0100

    Refactor ImageButton to remove caching and debug code
    Removed caching logic and VAT autocomplete fields from the ImageButton class. The code now directly creates thumbnail images without checking for cached versions. Debug-related functionality has also been eliminated, simplifying the image creation process while maintaining the core functionality of overlaying text and images.

--- LogicPOS.UI/Components/Common/Buttons/ImageButton.cs
+++ LogicPOS.UI/Components/Common/Buttons/ImageButton.cs
@@ -12,9 +12,6 @@ namespace LogicPOS.UI.Buttons
         private const int _BUTTON_INNER_BORDER = 7;
         private const int _BUTTON_TEXT_OVERLAY_INNER_MARGIN = 0;
         private const int _BUTTON_TEXT_ALPHA_OVERLAY = 250;
-        private readonly bool _useCachedImages = PreferenceParametersService.UseCachedImages;
-        private readonly bool _useVatAutocompletee = PreferenceParametersService.UseEuropeanVatAutoComplete;
-        private readonly string _pathCache = Convert.ToString(AppSettings.Paths.Cache);
 
         public ImageButton(ButtonSettings settings)
             : base(settings)
@@ -25,31 +22,21 @@ namespace LogicPOS.UI.Buttons
 
         public Widget CreateWidget()
         {
-            string stringImageFilename = _pathCache + _settings.Name + ".png";
             System.Drawing.Bitmap bitmap;
 
-            if (_useCachedImages && File.Exists(stringImageFilename))
-            {
-                //Load cached Thumbnail
-                bitmap = new System.Drawing.Bitmap(stringImageFilename);
-            }
-            else
-            {
-                //createThumbnail
-                bitmap = CreateThumbnail(_settings.Name,
-                                         _settings.BackgroundColor,
-                                         _settings.Text,
-                                         _settings.FontSize,
-                                         _settings.Image,
-                                         _settings.Overlay,
-                                         _settings.ButtonSize.Width,
-                                         _settings.ButtonSize.Height);
-            }
+            bitmap = CreateThumbnail(_settings.Name,
+                                     _settings.BackgroundColor,
+                                     _settings.Text,
+                                     _settings.FontSize,
+                                     _settings.Image,
+                                     _settings.Overlay,
+                                     _settings.ButtonSize.Width,
+                                     _settings.ButtonSize.Height);
 
-            //create pixBuf + gtkImage
+
             Gdk.Pixbuf pixBuf = Utils.ImageToPixbuf(bitmap);
             Image gtkImage = new Image(pixBuf);
-            //free resources
+
             bitmap.Dispose();
             pixBuf.Dispose();
 
@@ -65,81 +52,40 @@ namespace LogicPOS.UI.Buttons
                                                       int width,
                                                       int height)
         {
-            bool debug = false;
-
             System.Drawing.Size targetImageSize = new System.Drawing.Size(width - _BUTTON_INNER_BORDER * 2, height - _BUTTON_INNER_BORDER * 2);
-            string stringResolution = "";
 
-            //Create a Working Bitmap
             System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(targetImageSize.Width, targetImageSize.Height);
             System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(bitmap);
-            //Required else creates Bold Fonts Bug.....q afinal apenas tinha a ver c o nivel de transparencia da imagem e nao do code, umas horas a procura deste bug improvável...
+        
             graphics.Clear(color);
 
-
-            if (debug)
-            {
-                stringResolution = bitmap.HorizontalResolution + "x" + bitmap.VerticalResolution;
-                bitmap.Save(AppSettings.Paths.Temp + name + "_1_new_" + stringResolution + ".png");
-            };
-
-            //Render Image Over bitmap
             if (image != string.Empty)
             {
                 if (File.Exists(image))
                 {
 
                     System.Drawing.Image imageButton = new System.Drawing.Bitmap(image);
-                    //Log.Debug(string.Format("CreateThumbnail(): imageButton: {0}x{1} resize to targetImageSize: {2}x{3}", imageButton.Width, imageButton.Height, targetImageSize.Width, targetImageSize.Height));
-
-                    if (debug)
-                    {
-                        stringResolution = imageButton.HorizontalResolution + "x" + imageButton.VerticalResolution;
-                        imageButton.Save(AppSettings.Paths.Temp + name + "_2_image_" + stringResolution + ".png");
-                    };
-
 
                     imageButton = Utils.ResizeAndCrop(imageButton, targetImageSize);
                     System.Drawing.Graphics.FromImage(bitmap).DrawImage(imageButton, 0, 0);
 
-
-                    if (debug)
-                    {
-                        stringResolution = bitmap.HorizontalResolution + "x" + bitmap.VerticalResolution;
-                        bitmap.Save(AppSettings.Paths.Temp + name + "_3_image_resized_" + stringResolution + ".png");
-                    };
-
                 }
             }
 
-            //Render Overlay Over bitmap
             if (overlay != string.Empty)
             {
                 if (File.Exists(overlay))
                 {
 
                     System.Drawing.Image imageOverlay = new System.Drawing.Bitmap(overlay);
-                    //System.Drawing.Image imageOverlay = System.Drawing.Image.FromFile(overlay);
-
-                    if (debug)
-                    {
-                        stringResolution = imageOverlay.HorizontalResolution + "x" + imageOverlay.VerticalResolution;
-                        imageOverlay.Save(AppSettings.Paths.Temp + name + "_4_overlay_" + stringResolution + ".png");
-                    };
 
                     imageOverlay = Utils.ResizeAndCrop(imageOverlay, targetImageSize);
                     System.Drawing.Graphics.FromImage(bitmap).DrawImage(imageOverlay, 0, 0);
 
-                    if (debug)
-                    {
-                        stringResolution = bitmap.HorizontalResolution + "x" + bitmap.VerticalResolution;
-                        bitmap.Save(AppSettings.Paths.Temp + name + "_5_resized_" + stringResolution + ".png");
-                    };
 
                 }
             }
 
-            //Render Text Over bitmap
             if (labelText != string.Empty)
             {
                 System.Drawing.Rectangle transpRectangle = new System.Drawing.Rectangle();
@@ -149,14 +95,6 @@ namespace LogicPOS.UI.Buttons
                 transpRectangle.Y = targetImageSize.Height - transpRectangle.Height - _BUTTON_TEXT_OVERLAY_INNER_MARGIN * 2;
                 Utils.ImageTextOverlay(bitmap, labelText, transpRectangle, System.Drawing.Color.Black, "Arial", fontSize, _BUTTON_TEXT_ALPHA_OVERLAY);
 
-
-                if (debug) bitmap.Save(AppSettings.Paths.Temp + @"touchbuttonImage6_" + stringResolution + "_textoverlay.png");
-
-            }
-
-            if (_useCachedImages && !File.Exists(_pathCache + name + ".png"))
-            {
-                bitmap.Save(_pathCache + name + ".png");
             }
 
             return bitmap;
