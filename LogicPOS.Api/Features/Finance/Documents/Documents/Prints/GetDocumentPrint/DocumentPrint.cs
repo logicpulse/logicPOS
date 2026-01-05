61d4c796d7180415e6d689ab92ec322310bce68b
LucianoSMS <yanilson1996@gmail.com>
2026-01-05 17:52:41 +0100

    Implements thermal printing control and validation.
    Adds the IsThermalPrint field to track the type of printing on documents. Creates a query to obtain thermal printing information. Implements compatibility validation between document and printer, blocking incompatible prints and displaying alerts to the user. Adjusts print logging to consider the type of printer used, increasing traceability and preventing inconsistencies.

---
+++ LogicPOS.Api/Features/Finance/Documents/Documents/Prints/GetDocumentPrint/DocumentPrint.cs
@@ -0,0 +1,17 @@
+﻿using LogicPOS.Api.Features.Common;
+using System;
+
+namespace LogicPOS.Api.Entities
+{
+    public class DocumentPrint : ApiEntity
+    {
+        public string Designation { get; set; }
+        public string CopyNames { get; set; }
+        public int PrintCopies { get; set; }
+        public string PrintMotive { get; set; }
+        public bool SecondPrint { get; set; }
+        public Guid? DocumentId { get; set; }
+        public Guid? ReceiptId { get; set; }
+        public bool IsThermalPrint { get; set; }
+    }
+}
---
+++ LogicPOS.Api/Features/Finance/Documents/Documents/Prints/GetDocumentPrint/GetDocumentPrintQuery.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+using LogicPOS.Api.Entities;
+using MediatR;
+using System;
+
+namespace LogicPOS.Api.Features.Documents.Documents.GetDocumentPrint
+{
+    public class GetDocumentPrintQuery : IRequest<ErrorOr<DocumentPrint>>
+    {
+        public Guid DocumentId { get; set; }
+
+        public GetDocumentPrintQuery(Guid id)
+        {
+            DocumentId = id;
+        }
+    }
+}
